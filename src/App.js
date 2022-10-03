5553e6e97a01cc4cd5abfa12cd5fee9efebc0f7c
CristianMP04 <christian.marti.pernin@gmail.com>
2022-10-03 16:10:44 +0200

    gallery + search
    Funcionalidad del buscador funciona, ver porque no impre las imágenes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import Axios from 'axios';
+import { Input } from 'semantic-ui-react';
+import { Button } from 'semantic-ui-react';
 import './App.css';
 
 function App() {
+  const [searchText, setSearchText] = useState("");
+  const [result, setResult] = useState([]);
+  
+  
+  const callFlickr = ()=> {
+    
+    Axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=77650dbdae1464d7bd1a01c685f3ec93&text=${searchText}&format=json&nojsoncallback=1`)
+    .then((response)=> {
+      console.log(response.data);
+        setResult(response.data.results);
+    })
+  } 
+
+
+
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <div className="container">
+            <Input value={searchText} onChange={(e)=> setSearchText(e.target.value)}  placeholder='Search...' />
+            <Button type='submit' primary onClick={callFlickr}>Get Photo</Button>
+            {console.log(searchText)}
+         </div>
+         <div className="container">
+                <div class="row text-center text-lg-start">
+                    {result && result.map(() => {
+                        return (
+                            
+                                    <img class="img-fluid img-thumbnail d-block mb-4 h-100" src="https://live.staticflickr.com/{server-id}/{id}_{secret}_{size-suffix}.jpg"/>
+                                    
+                            
+                        )
+                    })}
+                </div>
+
+            </div>
+         
+          
+    </>
   );
 }
 
